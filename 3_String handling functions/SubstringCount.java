03d2863f459173e7dc7cbcff15589a198bda9243
Hmanjulat <manjulah002@gmail.com>
2025-05-21 22:45:16 +0530

    Update SubstringCount.java

--- 3_String handling functions/SubstringCount.java
+++ 3_String handling functions/SubstringCount.java
@@ -1,3 +1,7 @@
+Write a Java Program for Counting how many times a substring appears in a main string
+using user defined function countOccurrences()
+
+
 package A3;
 import java.util.Scanner;
 public class  SubstringCount {
