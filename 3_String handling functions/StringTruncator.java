8aeeedf7d32fed762dc2057f05bf276d7da7b8a6
Hmanjulat <manjulah002@gmail.com>
2025-05-21 22:44:33 +0530

    Update StringTruncator.java

--- 3_String handling functions/StringTruncator.java
+++ 3_String handling functions/StringTruncator.java
@@ -1,3 +1,5 @@
+Write a Java Program for Shortening a string to a specified length and adds an ellipsis using user
+defined function truncate()
 package A3;
 import java.util.Scanner;
 public class StringTruncator {
