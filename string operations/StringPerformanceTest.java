91b5014ff0a187f9fbeafc79412545dbc53e451e
Hmanjulat <manjulah002@gmail.com>
2025-05-21 20:04:15 +0530

    Update StringPerformanceTest.java

--- string operations/StringPerformanceTest.java
+++ string operations/StringPerformanceTest.java
@@ -1,14 +1,12 @@
+Develop a Java program to make a Performance Test on StringBuffer and StringBuilder for
+appending the string ‘AIET’ for 10000 times both in the StringBuffer and StringBuilder. Justify
+your answer which one is better.
 package A2;
-
 public class StringPerformanceTest {
-	
-
-	    public static void main(String[] args) {
-
-	        int iterations = 10000;
+	public static void main(String[] args) {
+          int iterations = 10000;
 	        String text = "AIET";
-
-	        // Measure time for StringBuffer
+            // Measure time for StringBuffer
 	        StringBuffer stringBuffer = new StringBuffer();
 	        long startBuffer = System.nanoTime();
 	        for (int i = 0; i < iterations; i++) {
