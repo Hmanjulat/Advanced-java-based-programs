db799498ec50fc8229c822606f979f9f676ecd88
Hmanjulat <manjulah002@gmail.com>
2025-05-21 21:32:49 +0530

    Update StringOperations.java

--- 3_String handling functions/StringOperations.java
+++ 3_String handling functions/StringOperations.java
@@ -1,11 +1,14 @@
+Develop a java program for performing various string operations with different string
+handling functions directed as follows:
+String Creation and Basic Operations, Length and Character Access, String Comparison, String
+Searching, Substring Operations , String Modification, Whitespace Handling, String Concatenation,
+String Splitting, StringBuilder Demo, String Formatting , Validate Email with contains( ) and
+startsWith() and endsWith()
 package A3;
-
 public class StringOperations {
-	
+public static void main(String[] args) {
 
-	    public static void main(String[] args) {
-
-	        // 1. String Creation and Basic Operations
+      // 1. String Creation and Basic Operations
 	        String str1 = "Hello";
 	        String str2 = "World";
 	        System.out.println("String 1: " + str1);
